646cc24c784b2703c28c89d28d0f3c40615a5385
juanManuelMontoya <juanpirry1396@hotmail.com>
2022-07-22 00:00:24 -0500

    Modules and routing config complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule)
+  },
+  {
+    path: 'game', 
+    loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule)
+  },
+  {
+    path: '**', 
+    pathMatch: 'full', 
+    redirectTo:'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
