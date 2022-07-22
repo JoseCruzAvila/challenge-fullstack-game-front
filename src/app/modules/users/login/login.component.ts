d980ef8683a4f76b4548b52b8ec20129527febc8
juanManuelMontoya <juanpirry1396@hotmail.com>
2022-07-22 11:43:09 -0500

    Firebase login config, Login design

--- src/app/modules/users/login/login.component.ts
+++ src/app/modules/users/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/shared/services/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService, private router: Router) { 
+    //private displayService: DisplayService
+  }
 
   ngOnInit(): void {
+    this.authService.isLoggedIn ? this.router.navigate(['game/home']): "";
+  }
+
+  signIn(userName: string, password: string) {
+    this.authService.SignIn(userName, password);
+  }
+
+  auth() {
+    this.authService.GoogleAuth();
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    apiKey: "AIzaSyA1a3yv2WYBlWWoafkdlzvRbxhBZm2OTuU",
+    authDomain: "cards-game-164a6.firebaseapp.com",
+    projectId: "cards-game-164a6",
+    storageBucket: "cards-game-164a6.appspot.com",
+    messagingSenderId: "760380725265",
+    appId: "1:760380725265:web:e325378594f0bb6277f9b0",
+    measurementId: "G-RE53K553EY"
+  },
   production: false
 };
 
